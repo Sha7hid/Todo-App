0391139b45b83b8908c13849abeabc28304095f7
sha7hid <shahidshafadshahad@gmail.com>
2023-01-02 18:39:32 +0530

    edit

--- todo/lib/screens/home.dart
+++ todo/lib/screens/home.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todo/constants/colors.dart';
 
+// gdfjgfjjufhjghkgh
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: tdBGColor,
       appBar: AppBar(
         backgroundColor: tdBGColor,
         elevation: 0,
@@ -29,7 +31,28 @@ class Home extends StatelessWidget {
         ),
       ),
       body: Container(
-        child: Text('This is home screen'),
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(0),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: tdBlack,
+                  size: 20,
+                ),
+                prefixIconConstraints:
+                    BoxConstraints(maxHeight: 25, minHeight: 20),
+                border: InputBorder.none,
+                hintText: 'Search',
+                hintStyle: TextStyle(color: tdGrey),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
